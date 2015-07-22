aba126f55890c000b53290e91b1d79c22349c27d
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-07-23 00:36:02 +0500

    Try to get 3 genre types for each movie

--- app/src/main/java/org/ogasimli/MovieBox/movie/MovieList.java
+++ app/src/main/java/org/ogasimli/MovieBox/movie/MovieList.java
@@ -94,29 +94,47 @@ public class MovieList {
         }
 
         public String getMovieGenre(List<String> genreIds) {
-            String valueGenre = null;
-            if (genreIds.size() != 0) {
-                String firstGenreId = genreIds.get(0);
-                String[] id = {"28", "12", "16", "35", "80", "99", "18", "10751", "14", "10769",
-                        "36", "27", "10402", "9648", "10749", "878", "10770", "53", "10752", "37"};
-                String[] value = {"Action", "Adventure", "Animation", "Comedy", "Crime",
-                        "Documentary", "Drama", "Family", "Fantasy", "Foreign", "History",
-                        "Horror", "Music", "Mystery", "Romance", "Science Fiction",
-                        "TV Movie", "Thriller", "War", "Western"};
-                //String[] id = context.getResources().getStringArray(R.array.genre_id);
-                //String[] value = context.getResources().getStringArray(R.array.genre_id_values);
-
-                for (int i = 0; i < id.length; i++) {
-                    if (firstGenreId.equals(id[i])) {
-                        valueGenre = value[i];
-                        break;
-                    }
-                }
-            } else {
-                valueGenre = "";
+            String firstGenre;
+            String secondGenre;
+            String thirdGenre;
+            String resultGenre = null;
+            if (genreIds.size() > 3) {
+                firstGenre = determineGenre(genreIds.get(0));
+                secondGenre = determineGenre(genreIds.get(1));
+                thirdGenre = determineGenre(genreIds.get(2));
+                resultGenre = firstGenre + ", " + secondGenre + ", " + thirdGenre;
+            }else if (genreIds.size() == 2){
+                firstGenre = determineGenre(genreIds.get(0));
+                secondGenre = determineGenre(genreIds.get(1));
+                resultGenre = firstGenre + ", " + secondGenre;
+            }else if (genreIds.size() == 1){
+                firstGenre = determineGenre(genreIds.get(0));
+                resultGenre = firstGenre;
             }
 
-            setMovieGenre(valueGenre);
+            setMovieGenre(resultGenre);
+            return resultGenre;
+        }
+
+        private String determineGenre(String genreId) {
+            String valueGenre = null;
+            String[] id = {"28", "12", "16", "35", "80", "99", "18", "10751", "14", "10769",
+                    "36", "27", "10402", "9648", "10749", "878", "10770", "53", "10752", "37"};
+            String[] value = {"Action", "Adventure", "Animation", "Comedy", "Crime",
+                    "Documentary", "Drama", "Family", "Fantasy", "Foreign", "History",
+                    "Horror", "Music", "Mystery", "Romance", "Science Fiction",
+                    "TV Movie", "Thriller", "War", "Western"};
+            //String[] id = context.getResources().getStringArray(R.array.genre_id);
+            //String[] value = context.getResources().getStringArray(R.array.genre_id_values);
+
+            for (int i = 0; i < id.length; i++) {
+                if (genreId.equals(id[i])) {
+                    valueGenre = value[i];
+                    break;
+                } else {
+                    valueGenre = "";
+                }
+            }
 
             return valueGenre;
         }
